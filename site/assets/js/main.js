f0eb7a42815452b85c8b0fe0e541d06f9f727a45
Norman <normanramadhan3@gmail.com>
2021-02-08 20:07:21 +0700

    Create login page (desktop)

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,9 +1,1 @@
-window.onload = function() {
-  if (window.jQuery) {  
-      // jQuery is loaded  
-      alert("Yeah!");
-  } else {
-      // jQuery is not loaded
-      alert("Doesn't Work");
-  }
-}
+console.log('test');
