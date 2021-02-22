8813ece7c92c31131af022fe1480bb2d96095044
Norman <normanramadhan3@gmail.com>
2021-02-22 08:08:06 +0700

    Adding toggle password

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -2,9 +2,21 @@ $(document).ready(function () {
   function handleSelect() {
     $('.form__input--select').selectize({ create: true, sortField: 'asc' });
   }
-  function handleStepForm() {    
+  function handleStepForm() {
     $('#stepForm').steps();
   }
+  function handleTogglePassword() {
+    $(".icon-password").click(function () {
+      $(this).toggleClass("active");
+      var password = $(this).prev(".form__input--password");
+      if (password.attr("type") === "password") {
+        password.attr("type", "text");        
+      } else {
+        password.attr("type", "password");        
+      }
+    });
+  }
   handleSelect();
-  handleStepForm()
+  handleStepForm();
+  handleTogglePassword();
 }); 
