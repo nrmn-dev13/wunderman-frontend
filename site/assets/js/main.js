51d2a7e84e729469b8b41615217e1da0ad09a94d
Norman <normanramadhan3@gmail.com>
2021-05-19 14:21:31 +0700

    Apply jquery step in installation guide page

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,36 +1,42 @@
-$(document).ready(function () {  
-  function handleStepForm() {
-    $('#stepForm').steps();
+$(document).ready(function () {
+  function handleFormStep() {
+    $('#formStep').steps();
+  }
+  function handleGuideStep() {    
+    $("#guideStep").steps({
+      transitionEffect: "slideLeft"
+    });
   }
   function handleTogglePassword() {
     $(".icon-password").click(function () {
       $(this).toggleClass("active");
       var password = $(this).prev(".form__input--password");
       if (password.attr("type") === "password") {
-        password.attr("type", "text");        
+        password.attr("type", "text");
       } else {
-        password.attr("type", "password");        
+        password.attr("type", "password");
       }
     });
   }
   function handleReadMore() {
-    $('.button--read-more').click(function(){
+    $('.button--read-more').click(function () {
       if ($(this).closest(".card--attention").hasClass('expanded')) {
-        $(this).html( "Read More" );
+        $(this).html("Read More");
       } else {
-        $(this).html( "Less" );
+        $(this).html("Less");
       }
       $(this).closest(".card--attention").toggleClass('expanded');
     })
   }
   function handleOpenNav() {
-    $('.toggle--nav').click(function(){
+    $('.toggle--nav').click(function () {
       $('.sidebar').toggleClass('show--nav');
     })
   }
-  
-  handleStepForm();
+
+  handleFormStep();
   handleTogglePassword();
   handleReadMore();
   handleOpenNav();
-}); 
+  handleGuideStep();
+});
