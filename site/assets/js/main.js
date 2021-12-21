a5bf85729f9a8dc365b64cfc5ef2d44d80b694da
Norman <normanramadhan3@gmail.com>
2021-12-22 06:19:08 +0700

    Adding slick js

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-  
+  function sliderHandler() {
+    $(".slider--about").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: $(".prev-arrow"),
+      nextArrow: $(".next-arrow"),
+      mobileFirst: true,
+      responsive: [
+        {
+           breakpoint: 480,
+           settings: "unslick"
+        }
+     ]
+    });
+  }
+  sliderHandler()
 });
