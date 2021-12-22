36efe8ce0d694e4a2c955b9b488229ce4ef82e13
Norman <normanramadhan3@gmail.com>
2021-12-22 07:22:29 +0700

    Create slider function

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -6,6 +6,22 @@ $(document).ready(function () {
       prevArrow: $(".prev-arrow"),
       nextArrow: $(".next-arrow"),
       mobileFirst: true,
+      infinite: false,
+      responsive: [
+        {
+           breakpoint: 480,
+           settings: "unslick"
+        }
+     ]
+    });
+    $(".slider--perks").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: $(".prev-arrow--perks"),
+      nextArrow: $(".next-arrow--perks"),
+      dots: true,
+      mobileFirst: true,
+      infinite: false,
       responsive: [
         {
            breakpoint: 480,
