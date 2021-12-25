ec7cbb63fad7c1a02927d01b74b810d750d973bf
Norman <normanramadhan3@gmail.com>
2021-12-25 12:25:59 +0700

    Adding function for nav slide

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   function sliderHandler() {
-    $(".slider--about").slick({
+    var $status = $('.slider-nav__counter');
+    var $slickElement = $('.slider--about');
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+      //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+      var i = (currentSlide ? currentSlide : 0) + 1;
+      $status.html('<span class="current-number">' + '0' +i + '</span>' + ' / ' + '0' +slick.slideCount);
+  });
+    $slickElement.slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       prevArrow: $(".prev-arrow"),
